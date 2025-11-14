db6236547b4dc084cb592b2641ecb3b7773f6a43
mynameisjhon <63317914+MyNameIsJhon@users.noreply.github.com>
2025-11-14 04:00:12 +0100

    update

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -73,6 +73,8 @@ int	main(int ac, char **av, char **envp)
 		path = getcwd(NULL, 0);
 		hello = mini_prompt(path, "mynameisjhon", "minishell");
 		command = mini_parser(hello);
+		if (ft_strcmp(command->program, "exit\n"))
+			exit(0);
 		find_prog(command);
 		/* printf("path: %s\n", result); */
 		/* command_print(command); */
