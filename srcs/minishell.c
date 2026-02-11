4913896420459dfae6d3267ebae6e1deea62ccf4
mynameisjhon <63317914+MyNameIsJhon@users.noreply.github.com>
2026-02-11 22:27:18 +0100

    some norme

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,41 +6,41 @@
 /*   By: jriga <jriga@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 16:03:45 by jriga             #+#    #+#             */
-/*   Updated: 2026/02/11 22:10:11 by jriga            ###   ########.fr       */
+/*   Updated: 2026/02/11 22:26:36 by jriga            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	print_cmds(t_command *command)
-{
-	t_redir	*redir;
-	int		i;
-
-	printf("\n---Printing prog---\n\n");
-	while (command)
-	{
-		redir = command->redirections;
-		printf("prog name: %s\n", command->com_splited[0]);
-		i = 0;
-		while (command->com_splited[i])
-		{
-			printf("  arg[%d]: %s\n", i, command->com_splited[i]);
-			i++;
-		}
-		while (redir)
-		{
-			printf("  redir type: %d\n", redir->type);
-			redir = redir->next;
-		}
-		if (command->exec_path)
-			printf("  exec_path: %s\n", command->exec_path);
-		else
-			printf("exec_path: (null)");
-		command = command->next;
-	}
-	printf("\n");
-}
+/* void	print_cmds(t_command *command) */
+/* { */
+/* 	t_redir	*redir; */
+/* 	int		i; */
+/**/
+/* 	printf("\n---Printing prog---\n\n"); */
+/* 	while (command) */
+/* 	{ */
+/* 		redir = command->redirections; */
+/* 		printf("prog name: %s\n", command->com_splited[0]); */
+/* 		i = 0; */
+/* 		while (command->com_splited[i]) */
+/* 		{ */
+/* 			printf("  arg[%d]: %s\n", i, command->com_splited[i]); */
+/* 			i++; */
+/* 		} */
+/* 		while (redir) */
+/* 		{ */
+/* 			printf("  redir type: %d\n", redir->type); */
+/* 			redir = redir->next; */
+/* 		} */
+/* 		if (command->exec_path) */
+/* 			printf("  exec_path: %s\n", command->exec_path); */
+/* 		else */
+/* 			printf("exec_path: (null)"); */
+/* 		command = command->next; */
+/* 	} */
+/* 	printf("\n"); */
+/* } */
 
 int	main(int ac, char **av, char **envp)
 {
