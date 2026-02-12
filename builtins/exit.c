27194f77a9f2d95e128f2e2d17b028a4f36421f1
Sikorski <gitlab-sikorski-work.nhten@isvalua.com>
2026-02-12 17:47:06 +0100

    feat(exit) now accepting minus and plus at the beginning, fix in case of longer than 256, still need some work

--- builtins/exit.c
+++ builtins/exit.c
@@ -17,6 +17,8 @@ static int	ft_isarray_onlydigit(char *str)
 	int	i;
 
 	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
 	while (str[i])
 	{
 		if (ft_isdigit(str[i]) == 0)
@@ -26,8 +28,22 @@ static int	ft_isarray_onlydigit(char *str)
 	return (EXIT_SUCCESS);
 }
 
+static long	handle_arg_toobig(long arg)
+{
+	int	nbr;
+	
+	if (arg < -256)
+		nbr = 256;
+	else if (arg > 256)
+		nbr = -256;
+	while (arg > 256 || arg < -256)
+		arg += nbr;
+	return (arg);
+}
+
 int	handle_exit_command(t_command *command, t_context *ctx)
 {
+	long	arg;
 
 	if (command->com_splited[1] && ft_isarray_onlydigit(command->com_splited[1]) == EXIT_FAILURE)
 	{
@@ -43,6 +59,9 @@ int	handle_exit_command(t_command *command, t_context *ctx)
 		printf("exit: too many arguments\n");
 		return (0);
 	}
+	arg = ft_atol(command->com_splited[1]);
+	if (arg > 256 || arg < - 256)
+		handle_arg_toobig(arg);
 	if (!ft_strcmp(command->com_splited[0], "exit"))//TODO Does this security makes any sense ?
 	{
 		context_free(&ctx);
